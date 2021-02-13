5ee6103f2a133ecf547390fff8f3372b4dba486e
pmalhotra <parveshh@gmail.com>
2021-02-13 13:41:44 +0000

    discriminated union

--- src/03-More Types/Union.ts
+++ src/03-More Types/Union.ts
@@ -14,3 +14,33 @@ const mycard: AnyCard = {
   cardType: "Debit", // so her we can select which card this is
 };
 
+/// Discriminated Union
+
+type SportsCar = {
+  make: string;
+  model: string;
+  kind: "sports";
+  topSpeed: number;
+};
+
+type Truck = {
+  make: string;
+  model: string;
+  towingCapacity: number;
+  kind: "carrier";
+};
+
+type Vehicle = SportsCar | Truck;
+
+function getTowingCapacity(vehicle: Vehicle): number {
+  // show the dotted paths not showing all properties
+  switch (vehicle.kind) {
+    case "carrier": // show the intellisense popup here
+      return vehicle.towingCapacity;
+    case "sports":
+      return 0;
+    default:
+      throw Error(`Unknown vehicle ${vehicle}`);
+    //vehicle.  /// show the never type here as now we have stripped away both types, this will be never
+  }
+}
